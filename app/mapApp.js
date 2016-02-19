854e37107ff9d8baa141932dddfc5f5f39e9f99b
danielrussellLA <danielrussell16@gmail.com>
2016-02-18 18:37:59 -0800

    [feat] google map sucessfully renders user current location

--- app/mapApp.js
+++ app/mapApp.js
@@ -1,6 +1,6 @@
 angular.module('mapsApp', [])
-.controller('MapCtrl', function ($scope) {
-    //
+.controller('MapCtrl', function ($scope, MapHelpers) {
+
     var mapOptions = {
         zoom: 15,
         center: new google.maps.LatLng(34.0219, -118.4814),
@@ -8,8 +8,43 @@ angular.module('mapsApp', [])
     };
 
     $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    MapHelpers.getUserCurrentLocation($scope.map);
 
 
+})
+.factory('MapHelpers', function(){
+  function getUserCurrentLocation(map){
+     var infoWindow = new google.maps.InfoWindow({map: map});
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+          var pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          };
+
+          infoWindow.setPosition(pos);
+          infoWindow.setContent('Location found.');
+          map.setCenter(pos);
+        }, function() {
+          handleLocationError(true, infoWindow, map.getCenter());
+        });
+      } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+      }
+
+      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
+      }
+  }
+
+  return {
+    getUserCurrentLocation: getUserCurrentLocation
+  };
 });
     //
     // $scope.markers = [];
