04e05cd4b0f81ed45b29bc54e35e8c0cb9113c40
simplejeff <jeffshen86@gmail.com>
2016-02-22 15:47:48 -0800

    [fix] signin handler completed and successfully routes to home upon successful sign in

--- app/submitCtrl.js
+++ app/submitCtrl.js
@@ -1,11 +1,32 @@
 angular.module('signup', [])
 
-.controller('submitCtrl', ['$scope', '$http', function ($scope, $http) {
+.controller('submitCtrl', ['$scope', '$http', '$state', function ($scope, $http, $state) {
 
   $scope.addUser = function(){
     console.log('NEW USER', $scope.newUser);
     $http.post('/signup', $scope.newUser).success(function(response){
       console.log('SUCCESSFUL POST TO SERVER');
+      $state.go('signin');
     });
   };
+
+  $scope.signin = function(){
+    console.log('signing in');
+    $http.post('/signin', $scope.user).success(function(response){
+      console.log('successful SIGN IN');
+      $state.go('home');
+    });
+  };
+
+  $scope.signup = function(){
+    console.log('signing up');
+    $http.post('/signup', $scope.newUser).success(function(response){
+      console.log('FINISHED SIGNING UP');
+      $state.go('home');
+    });
+  };
+
+
+
 }]);
+``
