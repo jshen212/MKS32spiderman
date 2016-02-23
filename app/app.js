0e2d465f3d1aa845cba44be8336e89ac8cee4e67
simplejeff <jeffshen86@gmail.com>
2016-02-23 10:48:51 -0800

    [edit] fixed paths using ngRoute

--- app/app.js
+++ app/app.js
@@ -1,36 +1,72 @@
 var app = angular.module('app', [
    'mapsApp',
    'signup',
-   'ui.router'
+   'ngRoute'
 ]);
 
 app.controller('cafeListCtrl', function($scope){
   $scope.selected = false;
   $scope.toggleCoffeeShopAppointments = function(){
     $scope.selected = !$scope.selected;
-  }
+  };
 });
 
-app.config(['$urlRouterProvider', '$stateProvider', function($urlRouterProvider, $stateProvider){
-  $urlRouterProvider.otherwise('/home');
+app.config(['$routeProvider', function($routeProvider){
+  $routeProvider
 
-  $stateProvider
-    .state('home', {
-      url: '/home',
-      templateUrl: 'templates/home.html',
-    })
-    .state('appointments', {
-      url: '/appointments',
-      templateUrl: 'templates/appointments.html'
-    })
-    .state('signin', {
-      url: '/signin',
-      templateUrl: 'templates/signin.html',
-      controller: 'submitCtrl'
-    })
-    .state('signup', {
-      url: '/signup',
-      templateUrl: 'templates/signup.html',
-      controller: 'submitCtrl'
-    });
+  .when('/home', {
+    templateUrl: 'templates/home.html'
+  })
+
+  .when('/appointments', {
+    templateUrl: 'templates/appointments.html'
+  })
+
+  .when('/signin', {
+    templateUrl: 'templates/signin.html',
+    controller: 'submitCtrl'
+  })
+
+  .when('/signup', {
+    templateUrl: 'templates/signup.html',
+    controller: 'submitCtrl'
+  })
+
+  .otherwise({
+    redirectTo: '/home'
+  });
 }]);
+
+
+
+
+
+
+
+
+
+
+// *********ui router code (we switched to ng-route to use tokens)*********
+// app.config(['$urlRouterProvider', '$stateProvider', function($urlRouterProvider, $stateProvider){
+//   $urlRouterProvider.otherwise('/home');
+//
+//   $stateProvider
+//     .state('home', {
+//       url: '/home',
+//       templateUrl: 'templates/home.html',
+//     })
+//     .state('appointments', {
+//       url: '/appointments',
+//       templateUrl: 'templates/appointments.html'
+//     })
+//     .state('signin', {
+//       url: '/signin',
+//       templateUrl: 'templates/signin.html',
+//       controller: 'submitCtrl'
+//     })
+//     .state('signup', {
+//       url: '/signup',
+//       templateUrl: 'templates/signup.html',
+//       controller: 'submitCtrl'
+//     });
+// }]);
